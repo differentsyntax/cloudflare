c3215d9e3d4e55ae5caa2fc9e86f732cfee499ae
Mridul <ma3388@drexel.edu>
2020-04-18 08:52:46 -0400

    minor edits

--- index.js
+++ index.js
@@ -5,22 +5,26 @@ event.respondWith(handleRequest(event.request))
 async function handleRequest(request) {
 try {
 	let url = new URL("https://cfw-takehome.developers.workers.dev/api/variants")
-	let req = new Request(url)
+	const req = new Request(url)
 	let div = 0
+
 	let res = fetch(req)
 			.then(response => response.json())
 			.then(data => {
+				//generating random number between 0 and 1
 				div = Math.random()
+				
+				//logic to redistribute traffic A/B testing style  
 				if (div < 0.5) {
 						url = data.variants[0]
-						
 				}
 				else {
 						url = data.variants[1]
-				}
+				} 
 				return fetch(url)
 		})
-		return res
+	//response to original fetch
+	return res
 	}
 catch (err) {
 	return new Response(err)
